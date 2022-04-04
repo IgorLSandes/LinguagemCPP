62210a2c199fc6f1420e935d9b18e0d4bad32853
Igor <igorlimasandes@hotmail.com>
2022-04-04 08:52:17 -0300

    Primeiro C++

---
+++ segundo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+int n1 = 0;
+int n2 = 0;
+int n3 = 0;
+
+std::cout << "Entre com um número\n";
+std::cin >> n1;
+std::cout << "Entre com outro número\n";
+std::cin >> n2;
+std::cout << "O resultado é: "<< n2+n1 <<"";
+return 0;
+
+}
